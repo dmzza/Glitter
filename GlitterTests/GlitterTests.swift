c118f55b5c6f4e76b4bcf61e74ff1d8f16047a40
David Mazza <david_mazza@apple.com>
2025-10-30 12:23:07 -0700

    Initial Commit

---
+++ GlitterTests/GlitterTests.swift
@@ -0,0 +1,16 @@
+//
+//  GlitterTests.swift
+//  GlitterTests
+//
+//  Created by David Mazza on 10/30/25.
+//
+
+import Testing
+
+struct GlitterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
